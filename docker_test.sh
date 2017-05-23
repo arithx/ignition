b1aebf19f9c091e9c8fcc8aedee3777a1061e6e1
Stephen Lowrie <stephen.lowrie@gmail.com>
2017-05-23 13:41:52 -0700

    Try docker instead of systemd on trusty

---
+++ docker_test.sh
@@ -0,0 +1,25 @@
+sudo apt-get update
+sudo apt-get install git
+
+git clone https://github.com/coreos/ignition
+
+cd ignition
+
+# since libblkid-dev:arm64 cannot be installed, spoof it
+if [ "${TARGET}" == "arm64" ]; then
+      echo "void blkid_new_probe_from_filename(void) {}" >> stub.c;
+      echo "void blkid_do_probe(void) {}"                >> stub.c;
+      echo "void blkid_probe_has_value(void) {}"         >> stub.c;
+      echo "void blkid_probe_lookup_value(void) {}"      >> stub.c;
+      echo "void blkid_free_probe(void) {}"              >> stub.c;
+      aarch64-linux-gnu-gcc-4.8 -c -o stub.o stub.c;
+      aarch64-linux-gnu-gcc-ar-4.8 cq libblkid.a stub.o;
+fi
+
+if [ "${TARGET}" == "amd64" ]; then
+      GOARCH="${TARGET}" sudo -E env "PATH=$PATH" ./test;
+elif [ "${TARGET}" == "arm64" ]; then
+      export CGO_LDFLAGS="-L ${PWD}";
+      GOARCH="${TARGET}" ./build;
+      file "bin/${TARGET}/ignition" | egrep 'aarch64';
+fi
